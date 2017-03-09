e6316744274a0bfc74655a7ca5675063649d550b
bandita19 <bandita.mohapatra1901@gmail.com>
2017-03-09 23:55:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,18 +4,23 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('article-one', function (req, res) {
  res.send('/article 1 is requested and will be served here');
 });
+
 app.get('/article-two', function (req, res) {
  res.send('article 2 is requested and will be served here');
 });
+
 app.get('/article-three', function (req, res) {
  res.send('article 3 is requested and will be served here');
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
