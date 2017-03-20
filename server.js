2bab6a4a87624c166ac9dfecb10f79a81c1dc7f3
bandita19 <bandita.mohapatra1901@gmail.com>
2017-03-20 16:48:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ var article = {
         </p>`
 };
 function createTemp(data){
+    var title=data.title;
+    var head=data.head;
+    var content=data.content;
     var htmltemp=`
     <html>
         <head>
