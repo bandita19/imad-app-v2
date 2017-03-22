667126ae6261af89d330224f810c74281b7455f0
bandita19 <bandita.mohapatra1901@gmail.com>
2017-03-22 20:19:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,6 +64,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/test-db',function(req,res){
+    // make a select request 
+    //return a response with the results
+});
+
 app.get('/:articleName', function (req, res) {
  var articleName=req.params.articleName;
  res.send(createTemp(articleName));
