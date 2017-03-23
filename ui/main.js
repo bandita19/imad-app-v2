ed75aa8f3c233ac170106ef6cebd39dc6cf58cc2
bandita19 <bandita.mohapatra1901@gmail.com>
2017-03-23 21:29:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,21 @@
 //Counter code
 var button = document.getElementById('counter');
-var counter=0;
 button.onclick = function () {
     
     //Make a request to the counter endpoint.
+    var request = new XMLHttpRequest();
     
-    //Capture the response and store it in a variable.
-    
-    //Render the variable in the correct span
-    counter=counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //Capture the response and store it in a variable
+    reques.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // TAKE SOME ACTION
+            if (request.status === 200) {
+                var counter = request.response.Text;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+        //NOT DONE YET
+    };
 };
