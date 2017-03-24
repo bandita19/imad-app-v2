0806c093b8e2b59051429854f252b3478ba14e0e
bandita19 <bandita.mohapatra1901@gmail.com>
2017-03-24 21:09:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ var submit = document.getElementById("submit_btn");
 submit.onclick = function () {
     //Make a request to the server and send the name
     //Capture the list of names and render it as a list
-    var names = ['names1', 'names2', 'names3', 'name4'];
+    var names = ['names1', 'names2', 'names3', 'names4'];
     var list = '';
     for(var i=0; i<names.length; i++){
         list += '<li>' + names[i] + '</li>';
